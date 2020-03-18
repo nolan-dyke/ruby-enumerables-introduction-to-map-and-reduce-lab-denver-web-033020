43fa510159beb3f5a45176791d00567611d5fda9
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-03-18 20:04:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
-# My Code here....
+def map_to_negativize(array)
+  result_array = []
+  index = 0 
+  while index < array.length do 
+    result_array << array[index] * -1
+    index += 1 
+  end 
+end 
